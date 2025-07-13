377d8b8dd716e45272b7e8252f41a3d6632e571e
Apshara <apshara.amiruzzaman@icloud.com>
2025-07-13 16:14:32 +0100

    Ball can jump

---
+++ Assets/Scripts/CollisionForVelocity.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CollisionForVelocity : MonoBehaviour
+{
+    [SerializeField] float delayBeforeDestruction = 0.5f;
+    //[SerializeField] GameObject velocity;
+
+    string correct_answer = "Vector";
+    string incorrect_answer = "Scalar";
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        MonoBehaviour background = FindFirstObjectByType<Background>();
+        SpriteRenderer mySpriteRenderer = background.GetComponent<SpriteRenderer>();
+
+        if (other.gameObject.tag == correct_answer)
+        {
+
+            mySpriteRenderer.color = Color.green;
+
+            //Invoke("SetVelocityActive", delayBeforeDestruction);
+            Invoke("DestroyGameObject", delayBeforeDestruction);
+
+
+        }
+        else if (other.gameObject.tag == incorrect_answer)
+        {
+            mySpriteRenderer.color = Color.red;
+        }
+    }
+
+    //private void SetVelocityActive()
+    //{
+     //   velocity.SetActive(true);
+    //}
+
+    private void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Jump : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 10;
+    SurfaceEffector2D mySurfaceEffector2D;
+    Rigidbody2D myRigidBody;
+
+    void Awake()
+    {
+        thrust.Enable();
+    }
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            StartThrusting();
+        }
+
+    }
+
+    void StartThrusting()
+    {
+        //mySurfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
+        //mySurfaceEffector2D.enabled = false;
+        transform.Translate(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+    }
+}
